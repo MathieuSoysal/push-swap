4b39007f702ecc7b1cb6c3336a1e08f1935ee6c8
hsoysal <mathieu.soysal@outlook.fr>
2024-04-27 14:50:20 +0200

    Add test for double linked list tail

--- test/runtest.c
+++ test/runtest.c
@@ -6,13 +6,14 @@
 /*   By: hsoysal <hsoysal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 00:45:31 by hsoysal           #+#    #+#             */
-/*   Updated: 2024/04/27 14:43:16 by hsoysal          ###   ########.fr       */
+/*   Updated: 2024/04/27 14:47:26 by hsoysal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "acutest.h"
 #include "structures/double_linked_list/test_double_linked_list_essentials.h"
 #include "structures/double_linked_list/test_double_linked_list_head.h"
+#include "structures/double_linked_list/test_double_linked_list_tail.h"
 #include "structures/linked_list/test_linked_list_with_content.h"
 #include "structures/linked_list/test_linked_list_with_node.h"
 #include "test_utils.h"
@@ -26,5 +27,6 @@ TEST_LIST = {
 	{"Linked List with content", test_linked_list_with_content},
 	{"Double Linked List Essentials", test_double_linked_list_essentials},
 	{"Double Linked List Head", test_double_linked_list_head},
+	{"Double Linked List Tail", test_double_linked_list_tail},
 	{NULL, NULL} //
 };
---
+++ test/structures/double_linked_list/test_double_linked_list_tail.h
@@ -0,0 +1,161 @@
+
+#ifndef TEST_DOUBLE_LINKED_LIST_TAIL_H
+# define TEST_DOUBLE_LINKED_LIST_TAIL_H
+
+# include "../../acutest.h"
+# include "double_linked_list.h"
+# include <stdio.h>
+
+void	test_double_linked_list_add_last_content1(void)
+{
+	t_double_linked_list	*list;
+	int						data;
+
+	list = double_linked_list_create();
+	data = 42;
+	double_linked_list_add_last_content(list, &data);
+	TEST_CHECK(list->tail != NULL);
+	double_linked_list_free(list, NULL);
+}
+
+void	test_double_linked_list_add_last_content2(void)
+{
+	t_double_linked_list	*list;
+	int						data;
+	int						data2;
+
+	list = double_linked_list_create();
+	data = 42;
+	double_linked_list_add_last_content(list, &data);
+	TEST_CHECK(*(int *)list->tail->content == 42);
+	data2 = 21;
+	double_linked_list_add_last_content(list, &data2);
+	TEST_CHECK(*(int *)list->tail->content == 21);
+	TEST_CHECK(*(int *)list->head->content == 42);
+	double_linked_list_free(list, NULL);
+}
+
+void	test_double_linked_list_peak_last_content1(void)
+{
+	t_double_linked_list	*list;
+	int						data;
+
+	list = double_linked_list_create();
+	data = 42;
+	double_linked_list_add_last_content(list, &data);
+	TEST_CHECK(*(int *)double_linked_list_peak_last_content(list) == 42);
+	double_linked_list_free(list, NULL);
+}
+
+void	test_double_linked_list_peak_last_content2(void)
+{
+	t_double_linked_list	*list;
+	int						data;
+	int						data2;
+
+	list = double_linked_list_create();
+	data = 42;
+	double_linked_list_add_last_content(list, &data);
+	data2 = 21;
+	double_linked_list_add_last_content(list, &data2);
+	TEST_CHECK(*(int *)double_linked_list_peak_last_content(list) == 21);
+	double_linked_list_free(list, NULL);
+}
+
+void	test_double_linked_list_pop_last_content1(void)
+{
+	t_double_linked_list	*list;
+	int						data;
+
+	list = double_linked_list_create();
+	data = 42;
+	double_linked_list_add_last_content(list, &data);
+	TEST_CHECK(*(int *)double_linked_list_pop_last_content(list) == 42);
+	TEST_CHECK(double_linked_list_peak_last_content(list) == NULL);
+	double_linked_list_free(list, NULL);
+}
+
+void	test_double_linked_list_pop_last_content2(void)
+{
+	t_double_linked_list	*list;
+	int						data;
+	int						data2;
+
+	list = double_linked_list_create();
+	data = 42;
+	double_linked_list_add_last_content(list, &data);
+	data2 = 21;
+	double_linked_list_add_last_content(list, &data2);
+	TEST_CHECK(*(int *)double_linked_list_pop_last_content(list) == 21);
+	TEST_CHECK(*(int *)double_linked_list_peak_last_content(list) == 42);
+	double_linked_list_free(list, NULL);
+}
+
+void	test_double_linked_list_remove_last1(void)
+{
+	t_double_linked_list	*list;
+	int						data;
+
+	list = double_linked_list_create();
+	data = 42;
+	double_linked_list_add_last_content(list, &data);
+	double_linked_list_remove_last(list, NULL);
+	TEST_CHECK(list->tail == NULL);
+	TEST_CHECK(list->head == NULL);
+	double_linked_list_free(list, NULL);
+}
+
+void	test_double_linked_list_remove_last2(void)
+{
+	t_double_linked_list	*list;
+	int						data;
+	int						data2;
+
+	list = double_linked_list_create();
+	data = 42;
+	double_linked_list_add_last_content(list, &data);
+	data2 = 21;
+	double_linked_list_add_last_content(list, &data2);
+	double_linked_list_remove_last(list, NULL);
+	TEST_CHECK(*(int *)list->tail->content == 42);
+	TEST_CHECK(*(int *)list->head->content == 42);
+	double_linked_list_remove_last(list, NULL);
+	TEST_CHECK(list->tail == NULL);
+	TEST_CHECK(list->head == NULL);
+	double_linked_list_free(list, NULL);
+}
+
+void	test_double_linked_list_remove_last3(void)
+{
+	t_double_linked_list	*list;
+	int						data;
+	int						data2;
+
+	list = double_linked_list_create();
+	data = 42;
+	double_linked_list_add_last_content(list, &data);
+	data2 = 21;
+	double_linked_list_add_last_content(list, &data2);
+	double_linked_list_remove_last(list, NULL);
+	TEST_CHECK(*(int *)list->tail->content == 42);
+	TEST_CHECK(*(int *)list->head->content == 42);
+	double_linked_list_remove_last(list, NULL);
+	TEST_CHECK(list->tail == NULL);
+	TEST_CHECK(list->head == NULL);
+	double_linked_list_free(list, NULL);
+}
+
+void	test_double_linked_list_tail(void)
+{
+	test_double_linked_list_add_last_content1();
+	test_double_linked_list_add_last_content2();
+	test_double_linked_list_peak_last_content1();
+	test_double_linked_list_peak_last_content2();
+	test_double_linked_list_pop_last_content1();
+	test_double_linked_list_pop_last_content2();
+	test_double_linked_list_remove_last1();
+	test_double_linked_list_remove_last2();
+	test_double_linked_list_remove_last3();
+}
+
+#endif // TEST_DOUBLE_LINKED_LIST_TAIL_H
