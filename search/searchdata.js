266cacea3e6c4f670b6086a1174dd8883f943028
MathieuSoysal <MathieuSoysal@users.noreply.github.com>
2024-04-28 22:51:10 +0000

    deploy: 9ddb4b3a875ec250d00e0b7ed4f559467b138a55

--- search/searchdata.js
+++ search/searchdata.js
@@ -2,26 +2,20 @@ var indexSectionsWithContent =
 {
   0: "abcdfghilmnoprstuvw",
   1: "dln",
-  2: "u",
-  3: "a",
-  4: "c"
+  2: "c"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "files",
-  3: "functions",
-  4: "pages"
+  2: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Classes",
-  2: "Files",
-  3: "Functions",
-  4: "Pages"
+  2: "Pages"
 };
 
