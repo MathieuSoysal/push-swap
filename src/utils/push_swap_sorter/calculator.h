f63a11b1377e5411c29e49c257e599c9c695f74c
hsoysal <mathieu.soysal@outlook.fr>
2024-05-11 05:07:33 +0200

    Update push_swap_calculate_push_swap_moves function in calculator.h

--- src/utils/push_swap_sorter/calculator.h
+++ src/utils/push_swap_sorter/calculator.h
@@ -6,7 +6,7 @@
 /*   By: hsoysal <hsoysal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 03:59:23 by hsoysal           #+#    #+#             */
-/*   Updated: 2024/05/11 04:08:21 by hsoysal          ###   ########.fr       */
+/*   Updated: 2024/05/11 05:07:25 by hsoysal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,9 @@
 	in the stack
  * @param stack The t_stack from push_swap
  * @param num The number to put in the stack
- *
+ * @return The number of moves,
+	positive if the element is near the top of the stack,
+    negative if the element is near the bottom of the stack
  */
 int	push_swap_calculate_push_swap_moves(t_stack *stack, int num);
 
