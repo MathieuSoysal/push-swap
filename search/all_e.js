784f64c9ed0ff53898ec6b6f508c20f507ab4ec0
MathieuSoysal <MathieuSoysal@users.noreply.github.com>
2024-05-11 01:06:54 +0000

    deploy: 8c1055a421e4570b722935e288f843d3fa4710b0

--- search/all_e.js
+++ search/all_e.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['s_5fpush_5fswap_5fstacks_0',['s_push_swap_stacks',['../structs__push__swap__stacks.html',1,'']]],
-  ['set_20workspace_20to_20this_20project_1',['Set workspace to this project',['../index.html#autotoc_md7',1,'']]],
-  ['src_20main_20c_20tt_2',['Build and Run with &lt;tt&gt;src/main.c&lt;/tt&gt;',['../index.html#autotoc_md5',1,'']]]
+  ['s_5flist_0',['s_list',['../structs__list.html',1,'']]],
+  ['s_5fpush_5fswap_5fstacks_1',['s_push_swap_stacks',['../structs__push__swap__stacks.html',1,'']]],
+  ['set_20workspace_20to_20this_20project_2',['Set workspace to this project',['../index.html#autotoc_md7',1,'']]],
+  ['src_20main_20c_20tt_3',['Build and Run with &lt;tt&gt;src/main.c&lt;/tt&gt;',['../index.html#autotoc_md5',1,'']]]
 ];
--- search/classes_2.js
+++ search/classes_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['s_5fpush_5fswap_5fstacks_0',['s_push_swap_stacks',['../structs__push__swap__stacks.html',1,'']]]
+  ['s_5flist_0',['s_list',['../structs__list.html',1,'']]],
+  ['s_5fpush_5fswap_5fstacks_1',['s_push_swap_stacks',['../structs__push__swap__stacks.html',1,'']]]
 ];
